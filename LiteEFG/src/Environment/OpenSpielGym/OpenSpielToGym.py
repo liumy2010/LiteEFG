c6f8b7133154f5976541adb897f238e36709dc8c
liumy2010 <1060071097@qq.com>
2024-11-05 10:12:54 -0500

    modify

--- LiteEFG/src/Environment/OpenSpielGym/OpenSpielToGym.py
+++ LiteEFG/src/Environment/OpenSpielGym/OpenSpielToGym.py
@@ -3,6 +3,7 @@ sys.path.append("../")
 import pyspiel
 import LiteEFG
 from OpenSpiel.OpenSpielToGameFile import OpenSpielEnv
+import numpy as np
 
 class TabularStrategy(LiteEFG.Graph):
     def __init__(self):
@@ -16,8 +17,16 @@ class OpenSpielGymEnv(OpenSpielEnv):
         super().__init__(game, traverse_type, regenerate, is_gym=True)
         self.graph = TabularStrategy()
         self.set_graph(self.graph)
-        print(LiteEFG.FileEnv.get_strategy(self, 1, self.graph.strategy))
+        x = LiteEFG.FileEnv.get_value(self, 1, self.graph.strategy)
+        print(x)
+        length = 0
+        for _ in x:
+            length += len(_[1])
+        w = np.zeros(length)
+        #LiteEFG.FileEnv.set_value(self, 1, self.graph.strategy, w)
+        print(LiteEFG.FileEnv.get_value(self, 1, self.graph.strategy))
+        print(LiteEFG.FileEnv.exploitability(self, self.graph.strategy))
 
 if __name__ == "__main__":
-    env = OpenSpielGymEnv(pyspiel.load_game("kuhn_poker"), regenerate=True)
+    env = OpenSpielGymEnv(pyspiel.load_game("kuhn_poker"))
     
